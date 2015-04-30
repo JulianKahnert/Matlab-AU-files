849a0be717cf815a1c104072facc81f9978aa355
Julian Kahnert <julian.kahnert@student.jade-hs.de>
2015-04-30 13:41:25 +0200

    First working version (compared with ffmpeg).

--- TGM_auwrite.m
+++ TGM_auwrite.m
@@ -1,4 +1,4 @@
-function []=TGM_auwrite(wdata,fs,szName)
+function []=TGM_auwrite(szName,y,fs)
 % function to do something usefull (fill out)
 % Usage [outParam]=TGM_auwrite(inParam)
 %
@@ -22,36 +22,53 @@ function []=TGM_auwrite(wdata,fs,szName)
 % Ver. 0.01 initial create                                   29-Apr-2015 JK
 
 % To-Do:
-%   * check if .wav extension is missing and attends it automatically
+%   * check if .au extension is missing and attends it automatically
 %   * check if reshape really increases performance
 %   * error message, if aufile could not be written
+%   * blockwise writing
 
 %------------Your function implementation here--------------------------- 
+
+
+
+% variable values
+[iSamples,iCH]  = size(y);
+iSamples_total  = iCH * iSamples;
+iEncoding       = 3;
+
+% szFormat        = 'uint32';
+szFormat        = 'int16';
+nbits           = 16;
+
+
+% fixed values
+szMagicNumber   = '.snd';
+szDataSize      = 'ffffffff';
+
+
+%% input checking
+
+if strcmp(szName(end-3),'.au')
+    szName = [szName '.au'];
+end
+
+
 %% write header
 
 [FID,msg] = fopen(szName,'w','b');
-fwrite(FID,int32('.snd'),'uchar');      % 0 magic number
-fwrite(FID,24,'uint32');                % 1 data offset
-% fwrite(FID,985750-28,'uint32');         % 2 data size
-fwrite(FID,hex2dec('ffffffff'),'uint32');         % 2 data size
-fwrite(FID,3,'uint32');                 % 3 encoding
-fwrite(FID,fs,'uint32');                % 4 sample rate
-fwrite(FID,1,'uint32');                 % 5 channels
-
+fwrite(FID,int32(szMagicNumber),'uchar');       % 0 magic number
+fwrite(FID,24,'uint32');                        % 1 data offset
+fwrite(FID,hex2dec(szDataSize),'uint32');       % 2 data size
+fwrite(FID,iEncoding,'uint32');                 % 3 encoding
+fwrite(FID,fs,'uint32');                        % 4 sample rate
+fwrite(FID,iCH,'uint32');                    % 5 channels
 
 
 %% write data
 
-% szFormat = 'uint32';
-szFormat= 'int16';
-nbits   = 16;
-
-
-nchan   = size(wdata,2);
-
-% quantiierung:
+% quantisation
 max_amp = 2^(nbits-1);
-quant_data = round(wdata*max_amp);
+quant_data = round(y*max_amp);
 if nbits == 8,
   % in order to fit number range -128...+127 into that of an unsigned char:
   quant_data = quant_data + 128; 
@@ -60,18 +77,20 @@ end
 % check for possible clipping:
 nclips = numel(find( quant_data<-max_amp | quant_data >=max_amp ));
 if nclips > 0,
-  warning('your data block exhibits %d clipped sample(s), of %d samples in total\n', nclips, ntotal);
+  warning('your data block exhibits %d clipped sample(s), of %d samples in total\n', nclips, iSamples_total);
   % no explicit clipping necessayr here, as clipping is
   % automatically performed by fwrite later 
 end
 
 % for a higher speed:  
 % in case of stereo data, reshape them into 1 long column  
-if nchan > 1,
-  quant_data = reshape(quant_data', ntotal, 1);
+if iCH > 1,
+  quant_data = reshape(quant_data', iSamples_total, 1);
 end
+
 fwrite(FID, quant_data, szFormat);
 fclose(FID);
+
 
 %--------------------Licence ---------------------------------------------
 % Copyright (c) <2015> Julian Kahnert
