81be8e932be852ad05d91609cc28e26548d15edb
Julian Kahnert <julian.kahnert@student.jade-hs.de>
2015-05-21 12:15:51 +0200

    Annotations fix 2: au_info + au_write

--- au_write.m
+++ au_write.m
@@ -1,4 +1,4 @@
-function au_write(szFilename,data,fs,szEncoding,vInterval)
+function au_write(szFilename, data, fs, vRange, szDatatype)
 %AU_WRITE Write audiodata in an au-file.
 %
 % AU_WRITE(szFilename,data,fs,szEncoding,vInterval)
@@ -24,7 +24,9 @@ function au_write(szFilename,data,fs,szEncoding,vInterval)
 %       'double'
 %   vInterval
 %
-%   See also: au_info, au_read, 
+% ATTENTION: second interval automatically set!
+%
+%   See also: au_info, au_read,
 
 %--------------------------------------------------------------------------
 % This project is licensed under the terms of the MIT license.
@@ -35,97 +37,111 @@ function au_write(szFilename,data,fs,szEncoding,vInterval)
 % Ver. 0.2.0 help update                                     06-May-2015 JK
 % Ver. 0.3.0 first mayor release                             19-May-2015 JK
 %--------------------------------------------------------------------------
-% To-Do:
-%   * check if .au extension is missing and appends it automatically
-%   * check if reshape really increases performance
-%   * error message, if aufile could not be written
-%--------------------------------------------------------------------------
 
 
 %% input check
 
 % defaul input settings
 szEncoding_default  = 'int16';
-vInterval_default   = [1 Inf];
-if nargin < 4 || isempty(szEncoding)
-    szEncoding = szEncoding_default;
+vRange_default   = [1 Inf];
+if nargin < 4 || isempty(vRange)
+    vRange = vRange_default;
 end
-if nargin < 5 || isempty(vInterval)
-    vInterval = vInterval_default;
+if nargin < 5 || isempty(szDatatype)
+    szDatatype = szEncoding_default;
 end
 
-% variable values
-[iSamples,iCH]  = size(data);
-iSamples_total  = iCH * iSamples;
+b1 = vRange(2)-vRange(1)+1 ~= size(data,1);
+b2 = any(vRange <= 0);
+b3 = vRange(1) > vRange(2);
+if ~any(vRange == Inf) && (b1 || b2 || b3)
+    error('Input arguments data and range not consistent.')
+end
+return;
+keyboard
+[szPath,szName,szExt]= fileparts(szFilename);
+if isempty(szExt) || ~strcmp(szExt,'.au')
+    szFilename = fullfile(szPath,[szName '.au']);
+end
 
-% {iEncoding, szEncoding, iBitsPerSample, fwritePrecission, szCompression, bSupported, szDescription}
-caEncoding = [];
-load(fullfile(which(fileparts(mfilename('fullpath'))),'encoding.mat'))
+% Datatype {iEncoding, fwritePrecission, iBitsPerSample, szCompression, bSupported, szDescription}
+stDetails = struct(...
+    'mu',       {1, '',        8,  'u-law',        false}, ...
+    'int8',     {2, 'bit8',    8,  'Uncompressed', true}, ...
+    'int16',    {3, 'bit16'    16, 'Uncompressed', true}, ...
+    'int24',    {4, 'bit24',   24, 'Uncompressed', true}, ...
+    'int32',    {5, 'bit32',   32, 'Uncompressed', true}, ...
+    'float32',  {6, 'float32', 32, 'Uncompressed', true}, ...
+    'float64',  {7, 'float64', 64, 'Uncompressed', true} ...
+    );
 
+iEncoding       = stDetails(1).(szDatatype);
+szFormat        = stDetails(2).(szDatatype);
+iBitsPerSample  = stDetails(3).(szDatatype);
 
-if ~exist(szFilename,'file') || all(vInterval == vInterval_default)
-    iRowEncoding    = strcmpi(szEncoding,caEncoding(:,2));
-    iEncoding       = caEncoding{iRowEncoding,1};
-    iBitsPerSample  = caEncoding{iRowEncoding,3};
-    szFormat        = caEncoding{iRowEncoding,6};
-
-    % fixed values
-    szMagicNumber   = '.snd';
-
-    [szPath,szName,szExt]= fileparts(szFilename);
-    if isempty(szExt) || ~strcmp(szExt,'.au')
-        szFilename = fullfile(szPath,[szName '.au']);
-    end
-
-    % write header, if file does not exist
-    fid  = fopen(szFilename,'w','b');
-    fwrite(fid,int32(szMagicNumber),'uchar');          % 0 magic number
-    fwrite(fid,24,                  'uint32');         % 1 data offset
-    fwrite(fid,intmax('uint32'),    'uint32');         % 2 data size
-    fwrite(fid,iEncoding,           'uint32');         % 3 encoding
-    fwrite(fid,fs,                  'uint32');         % 4 sample rate
-    fwrite(fid,iCH,                 'uint32');         % 5 channels
+if ~exist(szFilename,'file') || all(vRange == vRange_default)
+    iNumChannels    = size(data,2);
+    iDataOffset     = 24;
+    iDataSize       = 0;
+    writeHeader(szFilename,iDataOffset,iEncoding,fs,iNumChannels)
     
 else
-    % get information, if file exists
-    stInfo          = au_info(szFilename);
-    if stInfo.NumChannels ~= size(data,2)
-            error('Number of channels in existing file and input matrix dismatch!')
-    end
+    [stInfo, iDataOffset, iDataSize] = au_info(szFilename);
+    iNumChannels= stInfo.NumChannels;
     
-    iEncoding       = stInfo.Encoding;
-    iRowEncoding    = find([caEncoding{:,1}] == iEncoding);
-    iBitsPerSample  = caEncoding{iRowEncoding,3};
-    szFormat        = caEncoding{iRowEncoding,6};
-    
-    if vInterval(1) == Inf
-        % case: append samples
-        fid = fopen(szFilename,'a','b');
-    
-    else
-        % case: change samples in interval
-        if vInterval(2)-vInterval(1)+1 ~= size(data,1)
-            error('Number of samples in interval and rows of input matrix dismatch!')
-        end
-        fid = fopen(szFilename,'r+','b');
-        % define first byte in the desired interval and jump to it
-        iOffset = stInfo.DataOffset + ...
-            (vInterval(1)-1)*iBitsPerSample/8*stInfo.NumChannels;
-        fseek(fid,iOffset,'bof');
-        
-    end
 end
 
 
-%% write data
+%% open & write
 
-% for a higher speed:  
-% in case of stereo data, reshape them into 1 long column  
-if iCH > 1,
-  data = reshape(data', iSamples_total, 1);
+% for a higher speed
+if iNumChannels > 1,
+    data = reshape(data', iNumChannels * size(data,1), 1);
 end
 
-%#% falls int strcmp(sz(1:3),'int')
-fwrite(fid, data*2^(iBitsPerSample-1), szFormat);
+% open a file
+fid = fopen(szFilename,'r+','b');
+
+if all(vRange == vRange_default)    % case: new file
+    iOffset = iDataOffset;
+    
+elseif vRange(1) == Inf             % case: append data
+    iOffset = iDataOffset + iDataSize;
+    
+else                                % case: write interval
+    iOffset = iDataOffset + (vRange(1)-1)*iBitsPerSample/8*iNumChannels;
+    
+end
+
+% jump to offset
+fseek(fid,iOffset,'bof');
+
+% write data
+if strcmp(szDatatype(1:2),'in') % case of int*
+    fwrite(fid, data*2^(iBitsPerSample-1), szFormat);
+    
+else                            % case of float*
+    fwrite(fid, data, szFormat);
+    
+end
 fclose(fid);
 
+
+%% helper functions
+
+    function writeHeader(szFilename,iDataOffset,iEncoding,fs,iNumChannels)
+        % write header, if file does not exist
+        fid_header  = fopen(szFilename,'w','b');
+        if fid_header == -1
+            error('Can not open file.')
+        end
+        fwrite(fid_header,int32('.snd'),    'uchar');   % 0 magic number
+        fwrite(fid_header,iDataOffset,      'uint32');  % 1 data offset
+        fwrite(fid_header,intmax('uint32'), 'uint32');  % 2 data size
+        fwrite(fid_header,iEncoding,        'uint32');  % 3 encoding
+        fwrite(fid_header,fs,               'uint32');  % 4 sample rate
+        fwrite(fid_header,iNumChannels,     'uint32');  % 5 channels
+        fclose(fid_header);
+    end
+
+end
