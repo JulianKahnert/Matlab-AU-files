b730a833b695c97b8db9dfa3ae29498ebc95458b
Julian Kahnert <mail@juliankahnert.de>
2015-09-12 15:24:44 +0200

    Improvements in class

--- AUClass.m
+++ AUClass.m
@@ -23,11 +23,15 @@ classdef AUClass < handle
         iCurSample          = [];
     end
 
+    
+    properties (SetAccess = protected, GetAccess = public, Hidden)
+        iDataOffset = [];
+    end
+    
+    
     properties ( Access = private )
         
         fid         = [];
-        szFullPath  = [];
-        iDataOffset = [];
         iEncoding   = [];
         szFormat    = [];
         
@@ -40,8 +44,7 @@ classdef AUClass < handle
             'int32',    {5, 'bit32',   32, 'Uncompressed', true},  ...
             'float32',  {6, 'float32', 32, 'Uncompressed', true},  ...
             'float64',  {7, 'float64', 64, 'Uncompressed', true}   ...
-            );
-        
+            );    
     end
     
     
@@ -50,111 +53,91 @@ classdef AUClass < handle
     methods ( Access = public)
         
         function self = AUClass(szFilename)
-            self.szFullPath     = szFilename; %#% not the full path in every case!!
-            [~, self.Filename]  = fileparts(szFilename);
-            
-%             % get full path
-%             fid_tmp     = fopen(szFilename, 'r');
-%             self.szFullPath  = fopen(fid_tmp);
-%             fclose(fid_tmp);
-%             
-%             [szPath, szName, szExt]= fileparts(self.szFullPath);
-%             if isempty(szExt) || ~strcmp(szExt, '.au')
-%                 self.szFullPath = fullfile(szPath, [szName '.au']);
-%             end
-%             
-%             disp(self.szFullPath)
-%             keyboard
-
-        end  
-        
-        function open(self, szPermission, iNumChannels, fs, szDatatype)
-            if exist(self.szFullPath, 'file')
-                switch szPermission
-                    case 'read'
-                        self.fid = fopen(self.szFullPath, 'r', 'b');
-                    case 'write'
-                        self.fid = fopen(self.szFullPath, 'r+', 'b');
-                end
-                if self.fid == -1
-                    error('Can not open file.')
-                end
-                
-                % read header
-                magicnumber = fread(self.fid, 4, 'uint8', 0, 'b');
-                if ~all(magicnumber' == uint8('.snd'))
-                    error('Header of the file corrupt. Is it a au-file?')
-                end
-                dataOffset  = fread(self.fid, 1, 'uint32', 0, 'b');
-                dataSize    = fread(self.fid, 1, 'uint32', 0, 'b');
-                encoding    = fread(self.fid, 1, 'uint32', 0, 'b');
-                fs          = fread(self.fid, 1, 'uint32', 0, 'b');
-                iNumChannels= fread(self.fid, 1, 'uint32', 0, 'b');
-                
-                % change datasize to unkown value
-                if strcmp(szPermission, 'write') && dataSize ~= intmax('uint32')
-                    fseek(self.fid, 8, 'bof');
-                    fwrite(self.fid, intmax('uint32'), 'uint32'); % 2 data size
-                end
-                
-                caDatatypes = fieldnames(self.stDetails);
-                temp        = struct2cell(self.stDetails(1));
-                szDatatype  = caDatatypes{ [temp{:}] == encoding};
-                
-                % get file size
-                stFile = dir(self.szFullPath);
-                dataSize = stFile.bytes - dataOffset;
-                iNumSamples = dataSize / (self.stDetails(3).(szDatatype)/8) / iNumChannels;
-                
-            else
-                if strcmp(szPermission, 'read')
-                    error('Wrong permission for creating a new file.')
-                end
-                
-                if nargin < 3
-                    iNumChannels = 2;
-                    fprintf('\t==> chosen default number of channels: 2\n')
-                end
-                if nargin < 4
-                    fs = 44100;
-                    fprintf('\t==> chosen default sample rate: 44100 Hz\n')
-                end
-                if nargin < 5
-                    szDatatype = 'int16';
-                    fprintf('\t==> chosen default datatype: int16\n')
-                end
-                
-                iNumSamples   = 0;
-                dataOffset = 24;
-                
-                % write header
-                self.fid  = fopen(self.szFullPath, 'w+', 'b');
-                if self.fid == -1
-                    error('Can not open file.')
-                end
-                fwrite(self.fid, int32('.snd'),     'uchar');  % 0 magic number
-                fwrite(self.fid, dataOffset,       'uint32'); % 1 data offset
-                fwrite(self.fid, intmax('uint32'),  'uint32'); % 2 data size
-                fwrite(self.fid, self.stDetails(1).(szDatatype),    'uint32'); % 3 encoding
-                fwrite(self.fid, fs,                'uint32'); % 4 sample rate
-                fwrite(self.fid, iNumChannels,      'uint32'); % 5 channels
+            [szPath, ~, szExt] = fileparts(szFilename);
+            if ~strcmp(szExt, '.au')
+                error('Please choose a au-file!')
             end
             
-            fseek(self.fid, dataOffset, 'bof');
+            if isempty(szPath)
+                self.Filename = fullfile(pwd,szFilename);
+            else
+                self.Filename = szFilename;
+            end
+        end  
+        
+        function open(self, szPermission, varargin)%(self, szPermission, iNumChannels, fs, szDatatype)
+            % CASE: read
+            if strcmp(szPermission, 'read')
+                    self.fid = fopen(self.Filename, 'r', 'b');
+                    if self.fid == -1
+                        error('Can not open file.')
+                    end
+                    readHeader(self);
+                    
+            elseif strcmp(szPermission(1:5), 'write')
+                % CASE: write
+                if exist(self.Filename, 'file') && strcmp(szPermission, 'write')
+                    self.fid = fopen(self.Filename, 'r+', 'b');
+                    if self.fid == -1
+                        error('Can not open file.')
+                    end
+                    readHeader(self);
+                    
+                    % change datasize to unkown value
+                    fseek(self.fid, 8, 'bof');
+                    fwrite(self.fid, intmax('uint32'), 'uint32'); % 2 data size
+
+                % CASE: writenew
+                else
+                    if isempty(varargin)
+                        self.NumChannels = 2;
+                        fprintf('\t==> chosen default number of channels: %i\n', self.NumChannels)
+                    else
+                        self.NumChannels = varargin{1};
+                    end
+                    
+                    if length(varargin) < 2
+                        self.SampleRate = 44100;
+                        fprintf('\t==> chosen default sample rate: %i Hz\n', self.SampleRate)
+                    else
+                        self.SampleRate = varargin{2};
+                    end
+                    
+                    if length(varargin) < 3
+                        self.Datatype = 'int16';
+                        fprintf('\t==> chosen default datatype: %s\n', self.Datatype)
+                    else
+                        self.Datatype = varargin{3};
+                    end
+                    
+                    self.iDataOffset = 24;
+                    
+                    self.fid = fopen(self.Filename, 'w+', 'b');
+                    if self.fid == -1
+                        error('Can not open file.')
+                    end
+                    writeHeader(self);
+                end
+                
+            else
+                error('Permission unkown!')
+            end
+            
+            % get file size
+            stFile = dir(self.Filename);
+            dataSize = stFile.bytes - self.iDataOffset;
             
             % write properties
-            self.NumChannels    = iNumChannels;
-            self.SampleRate     = fs;
-            self.TotalSamples   = iNumSamples;
-            self.Duration       = iNumSamples/fs;
-            self.BitsPerSample  = self.stDetails(3).(szDatatype);
-            self.Datatype       = szDatatype;
+            self.iEncoding          = self.stDetails(1).(self.Datatype);
+            self.szFormat           = self.stDetails(2).(self.Datatype);
+            self.BitsPerSample      = self.stDetails(3).(self.Datatype);
+            self.CompressionMethod  = self.stDetails(4).(self.Datatype);
             
-            self.iDataOffset    = dataOffset;
-            self.iEncoding      = self.stDetails(1).(szDatatype);
-            self.szFormat       = self.stDetails(2).(szDatatype);
+            self.TotalSamples       = dataSize / (self.BitsPerSample/8) / self.NumChannels;
+            self.Duration           = self.TotalSamples/self.SampleRate;
             
-            self.iCurSample     = 1;
+            fseek(self.fid, self.iDataOffset, 'bof');
+            self.iCurSample         = 1;
         end
         
         function close(self)
@@ -178,14 +161,18 @@ classdef AUClass < handle
             
         end
         
-        function vSignal = read(self, iNumSamples)
-            if iNumSamples == Inf
+        function vSignal = read(self, varargin)
+            if ~isempty(varargin) && varargin{1} > self.TotalSamples-self.iCurSample+1
+                error('Not enough samples to read. Choose less samples!')
+            end
+            
+            if isempty(varargin)
                 seek(self,1)
                 iNumSamples = self.TotalSamples;
+            else 
+                iNumSamples = varargin{1};
             end
-            if iNumSamples > self.TotalSamples-self.iCurSample+1
-                error('Not enough samples to read. Chose less samples!')
-            end
+            
             
             % define length of the desired interval and read the samples
             iNum_smp= iNumSamples * self.NumChannels;
@@ -217,7 +204,8 @@ classdef AUClass < handle
             end
             
             if self.TotalSamples == self.iCurSample-1
-                self.TotalSamples = self.TotalSamples + iRow;
+                self.TotalSamples   = self.TotalSamples + iRow;
+                self.Duration       = self.TotalSamples/self.SampleRate;
             end
             self.iCurSample = self.iCurSample + iRow;
         end
@@ -228,9 +216,41 @@ classdef AUClass < handle
     methods ( Access = private)
         function delete(self)
             close(self)
-            disp('DESTRUCTOR CALLED!!') %#%
         end
         
+        function writeHeader(self)
+            fwrite(self.fid, int32('.snd'),     'uchar');  % 0 magic number
+            fwrite(self.fid, self.iDataOffset,  'uint32'); % 1 data offset
+            fwrite(self.fid, intmax('uint32'),  'uint32'); % 2 data size
+            fwrite(self.fid, self.stDetails(1).(self.Datatype), 'uint32'); % 3 encoding
+            fwrite(self.fid, self.SampleRate,   'uint32'); % 4 sample rate
+            fwrite(self.fid, self.NumChannels,  'uint32'); % 5 channels 
+        end
+        
+        function readHeader(self)
+            magicnumber = fread(self.fid, 4, 'uint8', 0, 'b');
+            if ~all(magicnumber' == uint8('.snd'))
+                error('Header of the file corrupt. Is it a au-file?')
+            end
+            self.iDataOffset    = fread(self.fid, 1, 'uint32', 0, 'b');
+            dataSize            = fread(self.fid, 1, 'uint32', 0, 'b'); %#ok not needed
+            encoding            = fread(self.fid, 1, 'uint32', 0, 'b');
+            self.SampleRate     = fread(self.fid, 1, 'uint32', 0, 'b');
+            self.NumChannels    = fread(self.fid, 1, 'uint32', 0, 'b');
+            
+            caDatatypes = fieldnames(self.stDetails);
+            temp        = struct2cell(self.stDetails(1));
+            szDatatype  = caDatatypes{ [temp{:}] == encoding};
+            
+            self.Datatype       = szDatatype;
+            
+        end
+        
+        function getNumSamples(self)
+            stFile  = dir(self.Filename);
+            dataSize= stFile.bytes - dataOffset;
+            iNumSamples = dataSize / (self.stDetails(3).(szDatatype)/8) / iNumChannels;
+        end
     end
     
     
